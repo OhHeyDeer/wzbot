f10321ab70af33b67310e8e387563a5181032b6b
Nick Timms <nicktimms1@hotmail.com>
2021-06-09 15:34:15 -0600

    Added Logic to the sortedKills function. Still work in progress and yet to test.

--- app.js
+++ app.js
@@ -8,18 +8,23 @@ const client = new Discord.Client();
 let matchData = [];
 let oldData = [];
 let totalKills = [];
+// const inputString = (prompt('Who is Playing today?')).split(' ');
 
 client.on('ready', () => {
     console.log('Bot is Running...');
+    
     runWZStats((data) => {
-        let solnitMatches = data[0];
-        let timmsMatches = data[1];
-        let ochoaMatches = data[2];
-        let scottMatches = data[3];
-        let laraMatches = data[4];
+
+        let solnitMatches = data[0]; // BS
+        let timmsMatches = data[1]; // NT
+        let ochoaMatches = data[2]; // JO
+        let scottMatches = data[3]; // SG
+        let laraMatches = data[4]; // JL
+
 
         matchData = [solnitMatches, timmsMatches, ochoaMatches, scottMatches, laraMatches];
         totalKills = [0,0,0,0,0];
+
         // Check each set of matches and add together the kills from only todays matches.
         for(let i = 0; i < matchData.length; i++) { // Iterate through the array of everyones matches
             let currentKills = 0;
@@ -30,7 +35,6 @@ client.on('ready', () => {
             }
             totalKills[i] += currentKills;
         }
-        console.log(totalKills);
     });
     
 });
@@ -41,10 +45,11 @@ client.on('ready', () => {
 
     const generalChannel = client.channels.cache.get(process.env.ZONE_CHAT_TOKEN);
 
-    let count = 0;
+    let oldTotal;
     setInterval(() => {
         
         oldData = matchData;
+        oldTotal = totalKills;
 
         runWZStats((data) => {
             let solnitMatches = data[0];
@@ -55,7 +60,6 @@ client.on('ready', () => {
 
             matchData = [solnitMatches, timmsMatches, ochoaMatches, scottMatches, laraMatches];
             console.log('DOING MATH');
-            // Logic for who is at the top
             
             
             // Who is playing:
@@ -107,73 +111,141 @@ client.on('ready', () => {
             // Have a copy of the kills array, reorder it to be asceding
             let killCopy = [...totalKills];
 
-            let sortedKills = killCopy.sort();
+            let sortedKills = killCopy.sort((a,b) => b-a);
 
-            let firstKills = killCopy.indexOf(sortedKills[0]);
-            let firstPlaceName = arrayOfNames[firstKills];
+            let firstKillsIndex = killCopy.indexOf(sortedKills[0]);
+            let secondKillsIndex;
+            let thirdKillsIndex;
+
+            let firstPlaceName = arrayOfNames[firstKillsIndex];
             let secondPlaceName;
             let thirdPlaceName;
-            let secondKills;
-            let thirdKills;
-            if (sortedKills[0] !== sortedKills[1]) {
-                secondKills = killCopy.indexOf(sortedKills[1]);
-                if (sortedKills[1] !== sortedKills[2]) {
-                    thirdKills = killCopy.indexOf(sortedKills[2]);
 
-                } else { // Tie for second place
+            let firstPlaceKills = sortedKills[0];
+            let secondPlaceKills = sortedKills[1];
+            let thirdPlaceKills = sortedKills[2];
+
+            if (sortedKills[0] !== sortedKills[1]) { // No Tie for First Place
+                
+                if (sortedKills[1] !== sortedKills[2]) { // No Tie for second place
+                    secondKillsIndex = killCopy.indexOf(sortedKills[1]); // Second is Second
+                    secondPlaceName = arrayOfNames[secondKillsIndex]; // Set the name to be correct
+
+                    if (sortedKills[2] !== sortedKills[3]) { // No Tie for 3rd place
+                        thirdKillsIndex = killCopy.indexOf(sortedKills[2]); // Third Is Third
+                        thirdPlaceName = arrayOfNames[thirdKillsIndex];
+                    } else { // Tie for third place
+                        let resetableKills = [...killCopy];
+
+                        let killIndexTemporary = resetableKills.indexOf(sortedKills[2]);
+                        resetableKills.splice(killIndexTemporary, 1 , -1); // Replace the kill value in the temp array with -1 to get second index
+                        let killIndexTemporary1 = resetableKills.indexOf(sortedKills[3]);
+
+                        thirdPlaceName = 'Tied Between: ' + (arrayOfNames[killIndexTemporary]) + ' and ' + (arrayOfNames[killIndexTemporary1]) + '!';
+                    }
+                } else { // Tie for Second place
                     let resetableKills = [...killCopy];
-                    resetableKills = resetableKills.splice(secondKills, 1, -1); // Replace with -1 to keep the length the same
-                    thirdKills = resetableKills.indexOf(sortedKills[2]);
-                    thirdPlaceName = arrayOfNames[thirdKills];
+
+                    let killIndexTemporary = resetableKills.indexOf(sortedKills[1]);
+                    resetableKills.splice(killIndexTemporary, 1, -1); // Replace the kill value in the temp array with -1 to get second index
+                    let killIndexTemporary1 = resetableKills.indexOf(sortedKills[2]);
+
+                    secondPlaceName = 'Tied Between: ' + (arrayOfNames[killIndexTemporary]) + ' and ' + (arrayOfNames[killIndexTemporary1]) + '!';
+
+                    if (sortedKills[2] !== sortedKills[3]) { // No tie for third place
+                        thirdKillsIndex = killCopy.indexOf(sortedKills[2]); // Third Is Third
+                        thirdPlaceName = arrayOfNames[thirdKillsIndex];
+                    } else { // Tie for third place
+                        let resetableKills = [...killCopy];
+
+                        let killIndexTemporary = resetableKills.indexOf(sortedKills[2]);
+                        resetableKills.splice(killIndexTemporary, 1, -1); // Replace the kill value in the temp array with -1 to get second index
+                        let killIndexTemporary1 = resetableKills.indexOf(sortedKills[3]);
+
+                        thirdPlaceName = 'Tied Between: ' + (arrayOfNames[killIndexTemporary]) + ' and ' + (arrayOfNames[killIndexTemporary1]) + '!';
+                    }
 
                 }
             } else { // Tie for first place
-                let resetableKills = [... killCopy];
-                resetableKills = resetableKills.splice(secondKills, 1, -1); // Replace with -1 to keep the length the same
-                secondKills = resetableKills.indexOf(sortedKills[1]);
-                secondPlaceName = arrayOfNames[secondKills];
+                let resetableKills = [...killCopy];
 
-                if (sortedKills[1] !== sortedKills[2]) {
-                    thirdKills = killCopy.indexOf(sortedKills[2]);
-                    thirdPlaceName = arrayOfNames[thirdKills];
+                let killIndexTemporary = resetableKills.indexOf(sortedKills[0]);
+                resetableKills.splice(killIndexTemporary, 1, -1); // Replace the kill value in the temp array with -1 to get second index
+                let killIndexTemporary1 = resetableKills.indexOf(sortedKills[1]);
 
-                } else { // 3 way tie for first place
-                    resetableKills = [...killCopy];
-                    resetableKills = resetableKills.splice(thirdKills, 1, -1); // Replace with -1 to keep the length the same
-                    thirdKills = resetableKills.indexOf(sortedKills[2]);
-                    thirdPlaceName = arrayOfNames[thirdKills];
+                firstPlaceName = 'Tied Between: ' + (arrayOfNames[killIndexTemporary]) + ' and ' + (arrayOfNames[killIndexTemporary1]) + '!';
 
+                if (sortedKills[1] !== sortedKills[2]) { // No tie for second place
+                    secondKillsIndex = killCopy.indexOf(sortedKills[1]);
+                    secondPlaceName = arrayOfNames[secondKillsIndex];
+
+                    if (sortedKills[2] !== sortedKills[3]) { // No tie for third place
+                        thirdKillsIndex = killCopy.indexOf(sortedKills[2]); // Third Is Third
+                        thirdPlaceName = arrayOfNames[thirdKillsIndex];
+                    } else { // Tie for third place
+                        let resetableKills = [...killCopy];
+
+                        let killIndexTemporary = resetableKills.indexOf(sortedKills[2]);
+                        resetableKills.splice(killIndexTemporary, 1, -1); // Replace the kill value in the temp array with -1 to get second index
+                        let killIndexTemporary1 = resetableKills.indexOf(sortedKills[3]);
+
+                        thirdPlaceName = 'Tied Between: ' + (arrayOfNames[killIndexTemporary]) + ' and ' + (arrayOfNames[killIndexTemporary1]) + '!';
+                    }
+
+                } else { // Tie for second place
+                    let resetableKills = [...killCopy];
+
+                    let killIndexTemporary = resetableKills.indexOf(sortedKills[1]);
+                    resetableKills.splice(killIndexTemporary, 1, -1); // Replace the kill value in the temp array with -1 to get second index
+                    let killIndexTemporary1 = resetableKills.indexOf(sortedKills[2]);
+
+                    secondPlaceName = 'Tied Between: ' + (arrayOfNames[killIndexTemporary]) + ' and ' + (arrayOfNames[killIndexTemporary1]) + '!';
+                    
+                    if (sortedKills[2] !== sortedKills[3]) { // No tie for third place
+                        thirdKillsIndex = killCopy.indexOf(sortedKills[2]); // Third Is Third
+                        thirdPlaceName = arrayOfNames[thirdKillsIndex];
+                    } else { // Three way tie
+                        let resetableKills = [...killCopy];
+
+                        let killIndexTemporary = resetableKills.indexOf(sortedKills[2]);
+                        resetableKills.splice(killIndexTemporary, 1, -1); // Replace the kill value in the temp array with -1 to get second index
+                        let killIndexTemporary1 = resetableKills.indexOf(sortedKills[3]);
+
+                        thirdPlaceName = 'Tied Between: ' + (arrayOfNames[killIndexTemporary]) + ' and ' + (arrayOfNames[killIndexTemporary1]) + '!';
+                    }
                 }
 
             }
 
             console.log(firstPlaceName, secondPlaceName, thirdPlaceName);
+            console.log(firstPlaceKills, secondPlaceKills, thirdPlaceKills);
             // Find the kill count that is highest and correspond it to the array of names
 
-
-
             // SEND EVERY 30 Minutes
-            console.log('SENDING');
+            console.log('Doing Math.');
 
-            const messageEmbed = new Discord.MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Kill Leaderboard')
-                .setDescription('This is a kill tracker for everyone on the server. Each day, every persons kills reset and begin again.')
-                .setThumbnail('https://i.ytimg.com/vi/Ebmiv5hfNrg/maxresdefault.jpg')
-                .addFields(
-                    { name: 'Todays Leader:', value: firstPlaceName },
-                    { name: '\u200B', value: '\u200B' },
-                    { name: firstPlaceName, value: firstKills },
-                    { name: secondPlaceName, value: secondKills },
-                    { name: thirdPlaceName, value: thirdKills },
-                )
-
-            
-            generalChannel.send(messageEmbed);
-
+            let count = 0;
+            if (JSON.stringify(oldTotal) !== JSON.stringify(totalKills) || count === 0 ) { // This is an issu
+                count ++;
+                console.log("Sending...");
+                const messageEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Kill Leaderboard')
+                    .setDescription('This is a kill tracker for everyone on the server. Each day, every persons kills reset and begin again.')
+                    .setThumbnail('https://i.ytimg.com/vi/Ebmiv5hfNrg/maxresdefault.jpg')
+                    .addFields(
+                        { name: 'Todays Leader:', value: firstPlaceName },
+                        { name: '\u200B', value: '\u200B' },
+                        { name: firstPlaceName, value: firstPlaceKills },
+                        { name: secondPlaceName, value: secondPlaceKills },
+                        { name: thirdPlaceName, value: thirdPlaceKills },
+                    )
+    
+                generalChannel.send(messageEmbed);
+            }
         });
 
     
-    }, 1800000); // Every 30 minutes
+    }, 60000); // Every 1 minutes
 
 })
